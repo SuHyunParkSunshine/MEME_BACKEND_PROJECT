110bdaabbd5d809c879656763bd5d3d1565f9f51
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-10-13 22:03:16 +0900

    2023.10.12_회원 등록 유효성 검사_expected content 오류 발생

--- meme/meme/src/test/java/project/meme/controller/MemberControllerTest.java
+++ meme/meme/src/test/java/project/meme/controller/MemberControllerTest.java
@@ -1,5 +1,7 @@
 package project.meme.controller;
 
+import static org.mockito.Mockito.when;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -30,7 +33,7 @@ public class MemberControllerTest {
     @Test
     @DisplayName("회원 가입 유효성 검사")
     @WithMockUser
-    public void checkRegister() {
+    public void checkRegister() throws Exception {
         // 요청 본문에 요청할 가짜 MemberDTO 객체 생성
         MembersDto membersDto = new MembersDto();
         membersDto.setUserName("test");
@@ -40,6 +43,17 @@ public class MemberControllerTest {
         membersDto.setConfirmPassword("1234");
         membersDto.setPhoneNumber("1234");
         membersDto.setUserAddress("testtesttt");
+
+        //memberService의 registerService 메서드 호출 시의 동작을 가짜, 모의(mock)로 설정
+        when(membersService.registerService(membersDto)).thenReturn(ResponseEntity.ok("회원가입이 완료되었습니다"));
+
+        // "/register" 엔드포인트로 POST요청 보내고, MemberDto 객체를 JSON 형식으로 전송
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .with(csrf()) //csrf 비활성화
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(membersDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+//                .andExpect(MockMvcResultMatchers.content().string("회원가입이 완료되었습니다"));
     }
 
     @Test
